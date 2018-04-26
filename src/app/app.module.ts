ed52e9cbfbc1fbcf7bdbe3e3526c5295da437e8f
tfermoso <tomas.fermoso@gmail.com>
2018-04-26 06:28:50 +0200

    Estado inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AttendentListComponent } from './attendent-list/attendent-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AttendentListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBtrumBogMSyLWc-lLQt78_ukAsO1ELn3k",
+    authDomain: "eventosapp-ede6a.firebaseapp.com",
+    databaseURL: "https://eventosapp-ede6a.firebaseio.com",
+    projectId: "eventosapp-ede6a",
+    storageBucket: "eventosapp-ede6a.appspot.com",
+    messagingSenderId: "910012131966"
+  }
 };
